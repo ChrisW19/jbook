0675ec319d4d48e140293f620181ffd8fe19cbad
ChrisW19 <chris.wiseman74@gmail.com>
2024-09-24 16:25:22 -0700

    Added code editor with basic css

--- src/index.tsx
+++ src/index.tsx
@@ -3,58 +3,85 @@ import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom/client';
 import { unpkgPathPlugin } from './plugins/unpkgPathPlugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
+import CodeEditor from './components/code-editor';
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
 
-const el = document.getElementById("root");
+const el = document.getElementById('root');
+
 const root = ReactDOM.createRoot(el!);
 
 const App = () => {
-    const ref = useRef<esbuild.Service | null>()
+  const ref = useRef<any>();
+  const iframe = useRef<any>();
+  const [input, setInput] = useState('');
 
-    const [input, setInput] = useState<string>('');
-    const [code, setCode] = useState<string>('');
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
+    });
+  };
+  useEffect(() => {
+    startService();
+  }, []);
 
-    const startService = async () => {
-        ref.current = await esbuild.startService({
-            worker: true,
-            wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
-        });
-    };
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
 
-    useEffect(() => {
-        startService();
-    }, []);
+    iframe.current.srddoc = html;
 
-    const onClick = async () => {
-        if (!ref.current) {
-            return;
-        }
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
+    });
 
-        const result = await ref.current.build({
-            entryPoints: ['index.js'],
-            bundle: true,
-            write: false,
-            plugins: [
-                unpkgPathPlugin(),
-                fetchPlugin(input)
-            ],
-            define: {
-                'process.env.env.NODE_ENV': '"production"',
-                global: 'window'
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+  };
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try{
+            eval(event.data);
+            }  catch (err) {
+                const root = document.querySelector('#root');
+                root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
+                throw err;
             }
-        })
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
 
-        setCode(result.outputFiles[0].text);
-    };
-
-    return (
-        <div>
-            <textarea value={input} onChange={e => setInput(e.target.value)} />
-            <div>
-                <button className="btn btn-primary" onClick={onClick}>Submit</button>
-            </div>
-            <pre>{code}</pre>
-        </div>
-    );
+  return (
+    <div>
+        <CodeEditor 
+            initialValue='const a=1;'
+            onChange={(value) => setInput(value)}/>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <iframe title="preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
+    </div>
+  );
 };
 
 root.render(<App />);
