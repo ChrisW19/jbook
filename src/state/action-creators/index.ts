ceb3dc3f7c8f1374a00e9c921d5013c41f7a648c
ChrisW19 <58227421+ChrisW19@users.noreply.github.com>
2024-09-27 11:40:39 -0700

    Added Redux and Store and Immer
    Did simple test on insert cell

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,43 @@
+import { ActionType } from "../action-types";
+import { Direction, UpdateCellAction, DeleteCellAction, MoveCellAction, InsertCellAction} from "../actions";
+import { CellTypes } from "../cell";
+
+export const updateCell = (id: string, content: string): UpdateCellAction => {
+    return{
+        type: ActionType.UPDATE_CELL,
+        payload: {
+            id,
+            content,
+        }
+    }
+};
+
+export const deletCell = (id: string): DeleteCellAction => {
+    return{
+        type:ActionType.DELETE_CELL,
+        payload: {
+            id
+        }
+    }
+    
+};
+
+export const moveCell = (id: string, direction: Direction): MoveCellAction => {
+    return{
+        type:ActionType.MOVE_CELL,
+        payload: {
+            id,
+            direction
+        }
+    }
+};
+
+export const insertCell = (id:string, cellType: CellTypes): InsertCellAction => {
+    return{
+        type: ActionType.INSERT_CELL,
+        payload: {
+            id,
+            type: cellType
+        }
+    }
+};
