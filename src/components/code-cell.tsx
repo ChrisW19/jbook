fc15042d8cd54eca4b952e9fec78ed0ccb29032c
ChrisW19 <58227421+ChrisW19@users.noreply.github.com>
2024-10-03 14:25:45 -0700

    Cleaned up code and moved styling to proper file

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,4 +1,4 @@
-import { useEffect} from 'react';
+import { useEffect } from 'react';
 import CodeEditor from './code-editor';
 import Preview from './preview';
 import Resizable from './resizable';
@@ -7,55 +7,45 @@ import { useActions } from '../hooks/use-actions';
 import { useTypedSelector } from '../hooks/use-typed-selector';
 import './code-cell.css';
 
-
 interface CodeCellProps {
-  cell: Cell
+  cell: Cell;
 }
 
-const CodeCell: React.FC<CodeCellProps> = ({cell}) => {
-  const {updateCell, createBundle} = useActions();
-  const bundle =  useTypedSelector((state) => state.bundles[cell.id]);
+const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
+  const { updateCell, createBundle } = useActions();
+  const bundle = useTypedSelector(state => state.bundles[cell.id]);
 
   useEffect(() => {
-    const timer = setTimeout(async () => {
+    const timer = setTimeout(() => {
       createBundle(cell.id, cell.content);
     }, 750);
 
-    return () => {
-      clearTimeout(timer);
-    }
-
+    return () => clearTimeout(timer);
   }, [cell.id, cell.content, createBundle]);
 
   return (
     <Resizable direction="vertical">
-      <div style={{ height: 'calc(100% - 10px)', display: 'flex', flexDirection: 'row' }}>
+      <div className="code-cell-container">
         <Resizable direction="horizontal">
           <CodeEditor
             initialValue={cell.content}
-            onChange={(value) => updateCell(cell.id, value)}
+            onChange={value => updateCell(cell.id, value)}
           />
         </Resizable>
-          <div className="progress-wrapper">
-            {
-              !bundle || bundle.loading
-              ?
-              (
-              
-                <div className="progress-cover">
-                  <progress className="progress is-small is-primary" max="100">
-                    Loading
-                  </progress>
-                </div>
-              
-              )
-            :
-            (<Preview code={bundle.code} error={bundle.err}/>)}
-          </div>
+        <div className="progress-wrapper">
+          {!bundle || bundle.loading ? (
+            <div className="progress-cover">
+              <progress className="progress is-small is-primary" max="100">
+                Loading
+              </progress>
+            </div>
+          ) : (
+            <Preview code={bundle.code} error={bundle.err} />
+          )}
+        </div>
       </div>
     </Resizable>
   );
 };
 
 export default CodeCell;
-
